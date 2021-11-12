4768e74049fb8815f67b82f564edca627b0935eb
William Rutkowski <budski82@users.noreply.github.com>
2021-11-12 18:51:13 -0500

    found it dummy

--- js/player.js
+++ js/player.js
@@ -1,4 +1,4 @@
-/* (function()=>
+(()=>
 {
     const document_element = document.documentElement;
     const display = document.getElementById('cvs').getContext('2d', {alpha:false});
@@ -6,7 +6,4 @@
     display.canvas.width = document_element.clientWidth - 32;
 
 
-})(); */
-
-const canvas = document.getElementById('cvs');
-const ctx = canvas.getContext('2d', {alpha: true});
+})();
