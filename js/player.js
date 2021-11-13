1b4f5a0542a86c3dca623b7605f0a238406442fc
William Rutkowski <budski82@users.noreply.github.com>
2021-11-12 19:21:34 -0500

    grande

--- js/player.js
+++ js/player.js
@@ -3,7 +3,27 @@
     const document_element = document.documentElement;
     const display = document.getElementById('cvs').getContext('2d', {alpha:false});
 
-    display.canvas.width = document_element.clientWidth - 32;
+    const world_width  = 480;
+    const world_height = 480;
 
+    draw =()=> 
+    {
+        display.fillStyle = '#8669';
+        display.fillRect(0 ,0 ,world_width, world_height);
+    }
 
-})();
+    loop =()=>
+    {
+        window.requestAnimationFrame(loop);
+        
+
+      draw();
+    }
+
+    display.canvas.width  = world_width;
+    display.canvas.height = world_height;
+
+    window.requestAnimationFrame(loop);
+
+})
+();
